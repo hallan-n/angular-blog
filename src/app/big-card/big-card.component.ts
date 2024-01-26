40769c460c3c626fd6befe99a61d8cd199dedc1f
hallan-n <hallanneves01@gmail.com>
2024-01-25 22:43:11 -0300

    implements components

--- src/app/big-card/big-card.component.ts
+++ src/app/big-card/big-card.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
--- src/app/small-card/small-card.component.ts
+++ src/app/small-card/small-card.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
